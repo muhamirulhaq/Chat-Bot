1f8ed1c5ef2fd1f0403338fc099f7930a874f1d8
Muhammad Amirul Haq <70356768+muhamirulhaq@users.noreply.github.com>
2020-12-21 05:10:50 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,24 @@
-import {Chat} from "./Chat.js";
+let yourChat, keyYourChat, botChat, word;
 
-const chat = new Chat();
-const ok = document.querySelector(".ok");
+function chatting(x) {
 
-ok.onclick = chat.response;
+    yourChat = prompt(x);
+    keyYourChat = ["assalamualaikum", "pagi", "siang", "sore", "malam", "hai", "halo", "namamu", "tinggal", "namaku", "terimakasih", "kabar", "tanya", "kak"];
+    botChat = ["Waalaikumsalam kak", "Pagi kak", "Siang kak", "Sore kak", "Malam kak", "Hai kak", "Halo kak", "Nama saya Belum diprogram kak", "Saya tinggal di internet kak", "Maaf kak, saya tidak tahu nama kakak siapa", "Sama-sama kak", "Alhamdulillah, baik kak", "Silahkan bertanya kak", "Iya?"];
+    word = yourChat.split(" ");
 
+    for(let i = 0; i < keyYourChat.length; i++) {
+        for(let j = 0; j < word.length; j++) {
+
+            if(word[j].toLowerCase() === keyYourChat[i]) {
+
+                chatting(botChat[i]);
+                i = keyYourChat.length;
+                break;
+
+            } else if(i == keyYourChat.length-1 && j == word.length-1) chatting("Maaf, kami tidak mengerti maksud kakak");
+        }
+    }
+};
+
+chatting(); 
