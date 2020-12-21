dc4ab29cdfe91b7210ef88d1bac79c5221f71bb1
Muhammad Amirul Haq <mirhaq2002@gmail.com>
2020-12-21 18:44:27 +0000

    new

--- assets/js/Chat.js
+++ assets/js/Chat.js
@@ -5,8 +5,8 @@ export class Chat {
         const input = document.querySelector("input");
         const output = document.querySelector(".result");
 
-        const keyword = ["assalamualaikum", "pagi", "siang", "sore", "malam", "hai", "halo", "namamu", "tinggal", "namaku", "terimakasih", "kabar", "tanya", "kak"];
-        const botChat = ["Waalaikumsalam kak", "Pagi kak", "Siang kak", "Sore kak", "Malam kak", "Hai kak", "Halo kak", "Nama saya belum diprogram kak", "Saya tinggal di internet kak", "Maaf kak, saya tidak tahu nama kakak siapa", "Sama-sama kak", "Alhamdulillah, baik kak", "Silahkan bertanya kak", "Iya?"];
+        const keyword = ["assalamualaikum", "pagi", "siang", "sore", "malam", "tolong", "ngapain", "hai", "halo", "nyambung", "namamu", "tinggal", "namaku", "terimakasih", "kabar", "tanya", "bukan", "hei", "iya", "cerdas", "kamu", "nama", "bodoh", "gila", "tolol", "saya", "dia", "pintar", "marah", "apa", "kenapa", "bagaimana", "dimana", "siapa", "bodo", "susah", "goblok"];
+        const botChat = ["Waalaikumsalam", "Pagi", "Siang", "Sore", "Malam", "Ada apa?", "Lagi makan", "Hai", "Halo", "Kamu yang gak nyambung", "Nama saya belum diprogram", "Saya tinggal di internet", "Tidak tahu", "Sama-sama", "Alhamdulillah, baik", "Silahkan bertanya", "Terus..", "Iya?", "Hmmm..", "Makasih", "Saya?", "Kenapa nama?", "Kamu?", "Siapa yang gila?", "Kamu tolol?", "Bodo amat", "Siapa?", "Saya? Terimakasih", "Tidak", "Tidak tahu", "Saya tidak tahu", "Ya begitu", "Di sini", "Hmmm..saya tidak tahu", "Kenapa?", "Susah kenapa?", "Golok?"];
 
         this.response = function () {
             
@@ -23,7 +23,11 @@ export class Chat {
 
                         break;
 
-                    } else if (i == keyword.length - 1 && j == word.length - 1 && input.value !== "") output.innerHTML = "*Pernyataan tidak dikenali";
+                    } else if (i == keyword.length - 1 && j == word.length - 1 && input.value !== "") {
+
+                        input.value = "";
+                        output.innerHTML = "Istighfar";
+                    }
                 }
             }
         };
